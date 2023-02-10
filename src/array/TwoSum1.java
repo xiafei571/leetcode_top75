1928db848ce3a8c664921e3d82bd1db830593cf9
XIA FEI <xiafei571@gmail.com>
2023-02-10 15:20:11 +0900

    O(n) two sum

--- src/array/TwoSum1.java
+++ src/array/TwoSum1.java
@@ -1,5 +1,20 @@
 package array;
 
-public class TwoSum1 {
+import java.util.HashMap;
+import java.util.Map;
 
+public class TwoSum1 {
+	class Solution {
+		public int[] twoSum(int[] nums, int target) {
+			// T:O(N)
+			Map<Integer, Integer> map = new HashMap<>();
+			for (int i = 0; i < nums.length; i++) {
+				if (map.containsKey(target - nums[i])) {
+					return new int[] { map.get(target - nums[i]), i };
+				}
+				map.put(nums[i], i);
+			}
+			return new int[2];
+		}
+	}
 }
