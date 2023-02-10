aa90a4c1e582f3eafcec5ab69eb78578b44fbb6a
XIA FEI <xiafei571@gmail.com>
2023-02-10 15:29:36 +0900

    easy

--- src/array/BestTimetoBuyandSellStock121.java
+++ src/array/BestTimetoBuyandSellStock121.java
@@ -1,5 +1,16 @@
 package array;
 
 public class BestTimetoBuyandSellStock121 {
+	//Time: O(N)
+	public int maxProfit(int[] prices) {
+		int profit = 0;
+		int buy = prices[0];
 
+		for (int price : prices) {
+			profit = Math.max(profit, price - buy);
+			buy = Math.min(buy, price);
+		}
+
+		return profit;
+	}
 }
