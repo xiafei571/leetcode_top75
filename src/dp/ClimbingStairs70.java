f5bcfb86b2b3b69ab6ce4faae53c2a1d653056d2
XIA FEI <xiafei571@gmail.com>
2023-02-10 15:59:29 +0900

    方法数量的话 应该是累加

--- src/dp/ClimbingStairs70.java
+++ src/dp/ClimbingStairs70.java
@@ -1,5 +1,25 @@
 package dp;
 
 public class ClimbingStairs70 {
+	public int climbStairs(int n) {
+		// dp[i] = max(dp[i-1], dp[i-2]) + 1
+		// Space可以优化到O(1) 
+		if (n == 0) {
+			return 0;
+		}
+
+		if (n == 1) {
+			return 1;
+		}
+
+		int dp[] = new int[n + 1];
+		dp[1] = 1;
+		dp[2] = 2;
+		for (int i = 3; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+
+		return dp[n];
+	}
 
 }
